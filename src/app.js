892751db8163efd35a104c83a3d688d1577dcb97
Rudy-Ran <ranmeng2333@163.com>
2018-12-09 11:12:07 +0800

    deleteAuthorInfo

--- src/app.js
+++ src/app.js
@@ -1,30 +1,9 @@
-/*
-* @Author: Rosen
-* @Date:   2018-01-19 17:40:57
-* @Last Modified by:   Rosen
-* @Last Modified time: 2018-01-19 17:52:40
-*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-class Component  extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            name:'Rudy'
-        }
-    }
-    render(){
-        // setTimeout(()=>{
-        //     this.setState({
-        //         name:'Rudy-Ran'
-        //     })
-        // },2000)
-        return <h1>I am {this.props.name}</h1>
-    }
-}
+
 ReactDOM.render(
-    <Component name="RRRRRRudy"/>,
+    <Component/>,
     document.getElementById('app')
 );
 
